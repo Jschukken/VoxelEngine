c9e821961b467f408ecf8dba428eb3e186d4ae0a
Jschukken <jschukken@gmail.com>
2017-09-09 23:49:47 +0200

    Alright, thats enough for today

--- src/Shader/StaticShader.java
+++ src/Shader/StaticShader.java
@@ -1,5 +1,17 @@
 package Shader;
 
-public class StaticShader {
+public class StaticShader extends ShaderProgram {
 
+	
+	private static final String vertexFile = "";
+	private static final String fragmentFile = "";
+	
+	public StaticShader(){
+		super(vertexFile, fragmentFile);
+	}
+	
+	@Override
+	protected void bindAttributes(){
+		
+	}
 }
