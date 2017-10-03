c36a4b6dea8b869a046dad8b1fa1a1025050b093
Drazarus <t.c.m.aerts@student.tue.nl>
2017-10-03 17:51:07 +0200

    Button class fully implemented
    Button class fully implemented. Getters created. mouseInButton(Button button) fixed, texture changes when hovering but not clicking.

--- src/Entities/Button.java
+++ src/Entities/Button.java
@@ -5,11 +5,26 @@ import Models.TexturedModel;
 import Textures.ModelTexture;
 
 public class Button extends TexturedModel {
-	float[] vertices;
+	private float[] vertices;
 
 	public Button(RawModel model, ModelTexture texture, float[] vert) {
 		super(model, texture);
 		vertices = vert;
 	}
-
+	
+	public float getLeftX() {
+		return vertices[0];
+	}
+	
+	public float getRightX() {
+		return vertices[6];
+	}
+	
+	public float getTopY() {
+		return vertices[1];
+	}
+	
+	public float getBotY() {
+		return vertices[4];
+	}
 }
