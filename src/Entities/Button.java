3465b99e0c5baa1163a9b226548ab44b337ce962
Drazarus <t.c.m.aerts@student.tue.nl>
2017-10-03 18:27:29 +0200

    Added comments

--- src/Entities/Button.java
+++ src/Entities/Button.java
@@ -12,18 +12,34 @@ public class Button extends TexturedModel {
 		vertices = vert;
 	}
 	
+	/**
+	 * Gets the X coordinate of the left edge of the button
+	 * @return the X coordinate of the left edge as a float
+	 */
 	public float getLeftX() {
 		return vertices[0];
 	}
 	
+	/**
+	 * Gets the X coordinate of the right edge of the button
+	 * @return the X coordinate of the right edge as a float
+	 */
 	public float getRightX() {
 		return vertices[6];
 	}
 	
+	/**
+	 * Gets the Y coordinate of the top edge of the button
+	 * @return the Y coordinate of the top edge as a float
+	 */
 	public float getTopY() {
 		return vertices[1];
 	}
 	
+	/**
+	 * Gets the Y coordinate of the bottom edge of the button
+	 * @return the Y coordinate of the bottom edge as a float
+	 */
 	public float getBotY() {
 		return vertices[4];
 	}
