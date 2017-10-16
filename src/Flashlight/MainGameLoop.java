228b28f3114afe9f9bb69df378d7d42b071e7196
Drazarus <t.c.m.aerts@student.tue.nl>
2017-10-16 12:20:35 +0200

    WIP

--- src/Flashlight/MainGameLoop.java
+++ src/Flashlight/MainGameLoop.java
@@ -97,43 +97,8 @@ public class MainGameLoop {
 				menuh.createMenus(loader);
 				state = "mainMenu";
 				GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
-				
-				// Difficulty level HUD
-				ModelTexture ducker = new ModelTexture(loader.loadTexture("Potato"));
-				GuiTexture gui = new GuiTexture(ducker , new Vector2f(-0.6f, 0.9f), new Vector2f(0.4f, 0.1f)) {
-					@Override
-					public void update() {
-						
-					}
-				};
-				
-				// Timer HUD
-				ModelTexture ducker2 = new ModelTexture(loader.loadTexture("Potato"));
-				GuiTexture gui2 = new GuiTexture(ducker2 , new Vector2f(0f, 0.9f), new Vector2f(0.15f, 0.1f));
-				
-				// End Point HP HUD
-				ModelTexture ducker3 = new ModelTexture(loader.loadTexture("Potato"));
-				GuiTexture gui3 = new GuiTexture(ducker3 , new Vector2f(0.6f, 0.9f), new Vector2f(0.4f, 0.1f)) {
-					@Override
-					public void update() {
-						this.setScale(new Vector2f(this.getMaxScale().x*((float) mapManager.destination.getHealthPoints() / (float) mapManager.destination.getMaxHealthPoints()), this.getScale().y));
-					}
-				};
-				
-				// Player HP HUD
-				ModelTexture ducker4 = new ModelTexture(loader.loadTexture("Potato"));
-				GuiTexture gui4 = new GuiTexture(ducker4 , new Vector2f(-0.6f, -0.9f), new Vector2f(0.4f, 0.1f)) {
-					@Override
-					public void update() {
-						this.setScale(new Vector2f(this.getMaxScale().x*((float) mapManager.camera.getHp() / (float) mapManager.camera.getMaxhp()), this.getScale().y));
-					}
-				};
-				
-				guis.add(gui);
-				guis.add(gui2);
-				guis.add(gui3);
-				guis.add(gui4);
 				guiRenderer = new GuiRenderer(loader);
+				guiRenderer.createHUD();
 				break;
 
 			case "mainMenu":
@@ -171,7 +136,7 @@ public class MainGameLoop {
 				mapManager.update();
 				checkPause();
 				mapManager.render();
-				guiRenderer.render(guis);
+				guiRenderer.render();
 				DisplayManager.updateDisplay();
 				break;
 
--- src/Guis/GuiRenderer.java
+++ src/Guis/GuiRenderer.java
@@ -1,5 +1,6 @@
 package Guis;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.lwjgl.opengl.GL11;
@@ -7,23 +8,30 @@ import org.lwjgl.opengl.GL13;
 import org.lwjgl.opengl.GL20;
 import org.lwjgl.opengl.GL30;
 import org.lwjgl.util.vector.Matrix4f;
+import org.lwjgl.util.vector.Vector2f;
 
+import Flashlight.MainGameLoop;
 import Models.RawModel;
 import RenderEngine.Loader;
+import Textures.ModelTexture;
 import ToolBox.MatrixMath;
 
 public class GuiRenderer {
 
 	private RawModel quad;
 	private GuiShader guishader;
+	private Loader loader;
+	private List<GuiTexture> guis = new ArrayList<GuiTexture>();
+	private List<GuiTexture> timer = new ArrayList<GuiTexture>();
 	
-	public GuiRenderer(Loader loader){
+	public GuiRenderer(Loader load){
 	float[] pos = {-1, 1, -1, -1, 1, 1, 1, -1};
-	quad = loader.loadToVao(pos);
+	quad = load.loadToVao(pos);
 	guishader = new GuiShader();
+	loader = load;
 	}
 	
-	public void render(List<GuiTexture> guis){
+	public void render(){
 		guishader.start();
 		GL30.glBindVertexArray(quad.getVaoID());
 		GL20.glEnableVertexAttribArray(0);
@@ -42,6 +50,14 @@ public class GuiRenderer {
 			GL11.glDrawArrays(GL11.GL_TRIANGLE_STRIP, 0, quad.getVertexCount());
 		}
 		
+		updateTimer();
+		for(GuiTexture gui : timer) {
+			GL13.glActiveTexture(GL13.GL_TEXTURE0);
+			GL11.glBindTexture(GL11.GL_TEXTURE_2D, gui.getTexture());
+			Matrix4f matrix = MatrixMath.createTransformationMatrix(gui.getPosition(), gui.getScale());
+			guishader.loadTransformation(matrix);
+			GL11.glDrawArrays(GL11.GL_TRIANGLE_STRIP, 0, quad.getVertexCount());
+		}
 		//disable alphablending
 		//GL11.glDisable(GL11.GL_BLEND);
 		GL11.glEnable(GL11.GL_DEPTH_TEST);
@@ -50,6 +66,67 @@ public class GuiRenderer {
 		guishader.stop();
 	}
 	
+	public GuiTexture drawZero(Loader loader, Vector2f pos, Vector2f scale) {
+		ModelTexture mTex = new ModelTexture(loader.loadTexture("Potato"));
+		GuiTexture number = new GuiTexture(mTex , pos, scale);
+		return number;
+	}
+	
+	public void createHUD() {
+		
+		// Difficulty level HUD
+		ModelTexture ducker = new ModelTexture(loader.loadTexture("Potato"));
+		GuiTexture gui = new GuiTexture(ducker , new Vector2f(-0.6f, 0.9f), new Vector2f(0.4f, 0.1f)) {
+			@Override
+			public void update() {
+				
+			}
+		};
+		
+		// Timer HUD
+		ModelTexture ducker2 = new ModelTexture(loader.loadTexture("Potato"));
+		GuiTexture gui2 = new GuiTexture(ducker2 , new Vector2f(0f, 0.9f), new Vector2f(0.15f, 0.1f));
+		
+		// End Point HP HUD
+		ModelTexture ducker3 = new ModelTexture(loader.loadTexture("Potato"));
+		GuiTexture gui3 = new GuiTexture(ducker3 , new Vector2f(0.6f, 0.9f), new Vector2f(0.4f, 0.1f)) {
+			@Override
+			public void update() {
+				this.setScale(new Vector2f(this.getMaxScale().x*((float) MainGameLoop.mapManager.destination.getHealthPoints() / (float) MainGameLoop.mapManager.destination.getMaxHealthPoints()), this.getScale().y));
+			}
+		};
+		
+		// Player HP HUD
+		ModelTexture ducker4 = new ModelTexture(loader.loadTexture("Potato"));
+		GuiTexture gui4 = new GuiTexture(ducker4 , new Vector2f(-0.6f, -0.9f), new Vector2f(0.4f, 0.1f)) {
+			@Override
+			public void update() {
+				this.setScale(new Vector2f(this.getMaxScale().x*((float) MainGameLoop.mapManager.camera.getHp() / (float) MainGameLoop.mapManager.camera.getMaxhp()), this.getScale().y));
+			}
+		};
+		
+		guis.add(gui);
+		guis.add(gui3);
+		guis.add(gui4);
+		
+	}
+	
+	public void updateTimer() {
+		String digits = timer.toString();
+		Vector2f pos = new Vector2f(0f, 0.9f);
+		while (digits.length() < 4) {
+			digits = "0" + digits;
+		}
+		for (int i = i; i < digits.length(); i++) {
+			switch (digits.charAt(i)) {
+				case 0:
+					timer.set(i, drawZero(loader, pos, new Vector2f(Vector2f.add(pos, new Vector2f(i,0), pos), 0.01f)));
+					
+				case 1:
+					timer[i]
+			}
+		}
+	}
 	
 	
 public void cleanUp(){
