8f3e37e63d60801e222bf708e4c4d517ec723018
Chiel Ton <c.ton@student.tue.nl>
2017-10-19 21:31:14 +0200

    added the checkValid again
    Joost knows why i got deleted

--- src/Map/MapEvaluation.java
+++ src/Map/MapEvaluation.java
@@ -92,7 +92,32 @@ public class MapEvaluation {
 	private static void checkValid() {
 		int spawnacces = 0; // amount of spawns that can reach the end point via paths
 		found = false;
-		characteristics.add((double) 0);
+		if (spawns == 0) {
+			characteristics.add((double) 0);
+			return;
+		}
+			// run lee to check if the map is valid
+			for (int i = 0; i < map.length; i++)
+				for (int j = 0; j < map[0].length; j++)
+					if (map[i][j] == 3) {
+						lee(i, j);
+						if (found) {
+							spawnacces++;
+						}
+						// set values back to basic
+						found = false;
+						for (int k = 0; k < map.length; k++)
+							for (int l = 0; l < map[0].length; l++)
+								if (map[k][l] == 4)
+									map[k][l] = 1;
+					}
+
+			// check if all spawns worked
+			if (spawnacces == spawns) {
+				characteristics.add((double) 1);
+			} else {
+				characteristics.add((double) 0);
+			}
 	}
 
 	/**
